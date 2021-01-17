a196b1b5a69ecf6c4811c7529de068a525ef03e2
uSasha <sasha.engineer@gmail.com>
2021-01-17 15:15:45 +0300

    config

---
+++ src/config/__init__.py
@@ -0,0 +1,19 @@
+import importlib
+import os
+import sys
+
+environment = os.environ.get('environment', 'not_specified')
+modules = ['base', environment]
+
+if 'pytest' in sys.argv[0]:
+    modules.append('test')
+
+for module in modules:
+    try:
+        local_settings = importlib.import_module(f'config.{module}')
+        globals().update(local_settings.__dict__)
+    except ImportError:
+        print(f'Failed to import settings.{module}')    # noqa
+    else:
+        if os.environ.get('DEBUG_SETTINGS'):
+            print(f'Imported settings.{module}')    # noqa
---
+++ src/config/base.py
@@ -0,0 +1,12 @@
+REDIRECT_URL = 'http://127.0.0.1:8080'
+SPOTIFY_SCOPE = 'user-library-read'
+
+META_PATH = '../data/meta.pkl'
+ANN_PATH = '../data/items.ann'
+DISLIKE_THRESHOLD = 0.8
+
+SEED_WEIGHT = 1
+LIKES_WEIGHT = 0.6
+DISLIKES_WEIGHT = 0.3
+
+ASGI_WORKERS = 1
